5402b5bb0b37e5fc101aa704b52275f45b6641e4
Sebastián Garrido Cood <137034165+Garridoc1985@users.noreply.github.com>
2025-12-21 06:39:34 -0300

    Update Script_Create_table_Patagonia V4 _PK_FK_Base_datos_patagonia_estructura y data..sql

--- Script_Create_table_Patagonia V4 _PK_FK_Base_datos_patagonia_estructura y data..sql
+++ Script_Create_table_Patagonia V4 _PK_FK_Base_datos_patagonia_estructura y data..sql
@@ -1,3 +1,4 @@
+
 CREATE DATABASE  IF NOT EXISTS `patagonia` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `patagonia`;
 -- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
@@ -866,3 +867,4 @@ DELIMITER ;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
 -- Dump completed on 2024-06-19 21:58:14
+
